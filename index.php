9596d41f538a9ac641bcee04d2762fc68894fb88
Metehan <sarikusmetehan645@gmail.com>
2023-06-07 11:03:51 +0200

    push

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 <?php echo get_header(); ?>
 
 <?php
+echo "<div id='nav'>";
 //hiermee haal je het menu gesaved onder "extra-menu" op
 echo wp_nav_menu( array( 'theme_location' => 'extra-menu' ) );
+
 ?>
+</div>
 
 <h1><?php the_title(); ?></h1>
 
 <p><?php the_content(); ?></p>
 <link ref="stylesheet" href="style.css">
-
-<p>HAllo</p>
-
